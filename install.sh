2e07866ea1c324e67117330ea002c79733f4cac3
Arnon Hacohen <58848496+arnon001@users.noreply.github.com>
2024-06-11 13:13:32 +0300

    Added SSL to CPPS
    Used the tutorial in the link: https://discord.com/channels/323290581063172096/703770816990412870/730997587041190039

--- install.sh
+++ install.sh
@@ -37,6 +37,7 @@ read -p "Do you want to run the game when the installation ends? (y/N): " run_ga
 
 
 if [[ $(uname) == "Linux" ]]; then
+    read -p "Do you want to setup SSL? (will not work if not using a domain!) (y/N): " ssl
     echo "Setting up the environment."
     sudo apt update
     sudo apt install docker.io git curl -y
@@ -95,6 +96,27 @@ MATCHMAKING_TIMEOUT=30" > .env
 
 echo "Done!"
 
+if [ "$ssl" == "y" ] || [ "$ssl" == "Y" ]; then
+    echo "Setting up SSL"
+    sudo apt install certbot -y
+    certbot certonly --standalone -d $hostname -d play.$hostname -d media.$hostname -d legacy.$hostname -d old.$hostname
+    sudo apt install nginx -y
+    sudo echo "server {
+    listen 443 ssl;
+    server_name play.$hostname old.$hostname legacy.$hostname media.$hostname;
+    ssl_certificate /etc/letsencrypt/live/$hostname/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/$hostname/privkey.pem;
+
+    location / {
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_pass http://localhost:8080/;
+        error_log /var/log/wand.log;
+    }
+}" > /etc/nginx/sites-available/wand
+    sudo ln -s /etc/nginx/sites-available/wand /etc/nginx/sites-enabled/
+
+fi
 
 if [ "$run_game" == "y" ] || [ "$run_game" == "Y" ]; then
     sudo docker-compose up
